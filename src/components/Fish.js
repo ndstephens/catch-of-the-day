d449a854c0810fb97ae42acc00b02f4919ea67a8
Nathaniel Stephens <ndstephens@gmail.com>
2018-10-21 14:47:31 -0400

    Creates logic to add fish to order state

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react'
 import { formatPrice } from '../helpers'
 
 class Fish extends Component {
+  orderAdd = () => {
+    this.props.addToOrder(this.props.index)
+  }
+
   render() {
     const { image, name, price, desc, status } = this.props.details
+    const isAvailable = status === 'available'
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
@@ -12,7 +17,9 @@ class Fish extends Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add To Cart</button>
+        <button disabled={!isAvailable} onClick={this.orderAdd}>
+          {isAvailable ? 'Add To Order' : 'Sold Out!'}
+        </button>
       </li>
     )
   }
