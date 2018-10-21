a587c4290a441ade92338cf3271087830f7e37f0
Nathaniel Stephens <ndstephens@gmail.com>
2018-10-20 22:36:15 -0400

    Creates AddFishForm, adds state to App, function to update state

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,20 @@ import Order from './Order'
 import Inventory from './Inventory'
 
 class App extends Component {
+  state = {
+    fishes: {},
+    order: {},
+  }
+
+  addFish = fish => {
+    // 1. take a copy of the existing state (so as to not mutate the original)
+    const fishes = { ...this.state.fishes }
+    // 2. add our new fish to that 'fishes' object
+    fishes[`fish${Date.now()}`] = fish
+    // 3. set the new 'fishes' object state
+    this.setState({ fishes })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +25,7 @@ class App extends Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
